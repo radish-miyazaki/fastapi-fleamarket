96eecb61af77125235c618d241382ed176499dd8
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-05 17:50:21 +0900

    impl basic read

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 
+from cruds import item as item_cruds
+
 
 app = FastAPI()
 
 
-@app.get('/test')
-async def example():
-    return {'message': 'Hello World'}
+@app.get('/items')
+async def find_all():
+    return item_cruds.find_all()
+
+
+@app.get('/items/{id}')
+async def find_by_id(id: int):
+    return item_cruds.find_by_id(id)
+
+
+@app.get('/items/')
+async def find_by_name(name: str):
+    return item_cruds.find_by_name(name)
