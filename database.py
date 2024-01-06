04ea3c127da992debf7b7a81730f349c3bf0d008
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-01-06 17:54:29 +0900

    setup to connect database & define model

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://fastapiuser:fastapipass@localhost:5432/fleamarket'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
